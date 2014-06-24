83245b1441cdabdfad239e5f1831f7210e20eaa9
Marek Kotewicz <marek.kotewicz@gmail.com>
2014-06-24 20:45:41 +0200

    new layout - the beginning

--- app.js
+++ app.js
@@ -2,77 +2,32 @@ angular.module('tipping', ['ui.bootstrap','ui.utils','ui.router','ngAnimate']);
 
 angular.module('tipping').config(function($stateProvider, $urlRouterProvider) {
 
-    $stateProvider.state('index', {
-        templateUrl: 'partial/index/index.html'
-    });
-    $stateProvider.state('index.list', {
-        url: '/list',
-        views: {
-            content: {
-                templateUrl: 'partial/list/list.html'
-            }
-        }
+    var registerSubstateInView = function (state, substates) {
+        substates.forEach(function (substate) {
+            var hash = {};
+            hash.url = substate.url;
+            hash.views = {};
+            hash.views[substate.view] = {
+                templateUrl: substate.templateUrl
+            };
+            $stateProvider.state(state + '.' + substate.state, hash);
+        });
+    };
 
+    $stateProvider.state('leftbar', {
+        templateUrl: 'partial/leftbar/leftbar.html'
     });
-    $stateProvider.state('index.quote_details', {
-        url: '/:address',
-        views: {
-            content: {
-                templateUrl: 'partial/quote_details/quote_details.html'
-            }
-        }
-    });
-    $stateProvider.state('index.help', {
-        url: '/help/:p',
-        views: {
-            content: {
-                templateUrl: 'partial/help/help.html'
-            }
-        }
 
-    });
-    $stateProvider.state('index.transactions', {
-        url: '/transactions/:p',
-        views: {
-            content: {     
-                templateUrl: 'partial/transactions/transactions.html'
-            }
-        }
-    });
-    $stateProvider.state('index.notifications', {
-        url: '/notications/:p',
-        views: {
-            content: {
-                templateUrl: 'partial/notifications/notifications.html'
-            }
-        }
-    });
-    $stateProvider.state('index.account', {
-        url: '/account/:address',
-        views: {
-            content: {
-                templateUrl: 'partial/account/account.html'
-            }
-        }
-    });
-    $stateProvider.state('index.add_quote', {
-        url: '/add_quote/:p',
-        views: {
-            content: {
-                templateUrl: 'partial/add_quote/add_quote.html'
-            }
-        }
-    });
-    $stateProvider.state('index.about', {
-        url: '/about/:p',
-        views: {
-            content: {
-                templateUrl: 'partial/about/about.html'
-            }
-        }
-    });
+    registerSubstateInView('leftbar', [{
+        url: '/dashboard',
+        state: 'dashboard',
+        view: 'main',
+        templateUrl: 'partial/dashboard/dashboard.html'
+    }]);
+
+    
     /* Add New States Above */
-    $urlRouterProvider.otherwise('/list');
+    $urlRouterProvider.otherwise('/dashboard');
 
 });
 
---
+++ partial/dashboard/dashboard.js
@@ -0,0 +1,4 @@
+angular.module('tipping').controller('DashboardCtrl',function($scope){
+
+
+});
