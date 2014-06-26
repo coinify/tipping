9a492052ea0aa3916afac9cb7a15b1d3fdb97879
Marek Kotewicz <marek.kotewicz@gmail.com>
2014-06-26 07:30:02 +0200

    leftbar menu buttons

--- partial/leftbar/leftbar.js
+++ partial/leftbar/leftbar.js
@@ -1,4 +1,21 @@
 angular.module('tipping').controller('LeftbarCtrl',function($scope){
 
-
+    $scope.sections = [{
+        title: 'Dashboard',
+        image: '',
+        link: ''
+    },{
+        title: 'Wallet',
+        image: '',
+        link: ''
+    },{
+        title: 'Reputation',
+        image: '',
+        link: ''
+    },{
+        title: 'Settings',
+        image: '',
+        link: ''
+    }];
 });
+
