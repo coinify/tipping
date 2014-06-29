23995ce62b7926a22e10838e1e7e00591da35ab4
Marek Kotewicz <marek.kotewicz@gmail.com>
2014-06-30 00:57:20 +0200

    chat beginning

--- public/partial/tab/tab.js
+++ public/partial/tab/tab.js
@@ -1,4 +1,13 @@
 angular.module('tipping').controller('TabCtrl',function($scope, $stateParams, Chat){
     $scope.channel = $stateParams.channel;
+    $scope.messages = [];
+    
+    Chat.onMessage( function (message) {
+        $scope.messages.push(message);
+    });
 
+    $scope.sendMessage = function () {
+        Chat.sendMessage( $scope.inputMessage);
+        $scope.inputMessage = '';
+    };
 });
