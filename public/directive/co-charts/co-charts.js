c0f6d22dfddc76823e5626d955dbdb98878e0315
Marek Kotewicz <marek.kotewicz@gmail.com>
2014-07-11 15:12:33 +0200

    mockup charts

--- public/directive/co-charts/co-charts.js
+++ public/directive/co-charts/co-charts.js
@@ -1,23 +1,45 @@
 angular.module('tipping').directive('coCharts', function() {
 	return {
 		restrict: 'E',
-		replace: true,
 		scope: {
 
 		},
 		templateUrl: 'directive/co-charts/co-charts.html',
 		link: function(scope, element, attrs, fn) {
                     scope.config = {
-                        innerRadius: '60%'
+                        innerRadius: '65%',
+                        tooltips: false,
+                        labels: false
                     };
 
-                    scope.data = {
-                        data: [{
-                            y: [100]
-                        },{
-                            y: [150]
-                        }]  
-                    };
+                    scope.charts = _.map([{
+                        title: 'Boost posts',
+                        value: 63
+                    },{
+                        title: 'Your rate',
+                        value: 34
+                    },{
+                        title: 'Your profit',
+                        value: 81
+                    },{
+                        title: 'Like post',
+                        value: 10
+                    },{
+                        title: 'Share post',
+                        value: 40
+                    }], function (object) {
+                        return {
+                            data: {
+                                data: [{
+                                    y: [object.value]        
+                                }, {
+                                    y: [100 - object.value]
+                                }]
+                            },
+                            title: object.title,
+                            value: object.value
+                        };
+                    });
 		}
 	};
 });
